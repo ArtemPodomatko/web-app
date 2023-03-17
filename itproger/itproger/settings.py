8f72bd3b0dcd6b42d5280bd9a61923ad84c10ffb
ArtemPodomatko <tema.podomatko@mail.ru>
2023-03-17 22:31:32 +0300

    new apps, models

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
